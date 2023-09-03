7c7f59b06492d6fa9a34f2370b48ad38248056d8
Leonardo Diniz <leonardodiber@gmail.com>
2023-09-02 22:51:48 -0300

    initial commit
    fix module name
    
    initial commit
    
    initial commit

---
+++ internal/infra/web/order_handler.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/leodiberdev/desafio-clean-arch/internal/dto"
+	"github.com/leodiberdev/desafio-clean-arch/internal/entity"
+	"github.com/leodiberdev/desafio-clean-arch/internal/usecase"
+	"github.com/leodiberdev/desafio-clean-arch/pkg/events"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	EventDispatcher events.EventDispatcherInterface,
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   EventDispatcher,
+		OrderRepository:   OrderRepository,
+		OrderCreatedEvent: OrderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto dto.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent, h.EventDispatcher)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
